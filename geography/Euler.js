7dcbfa8975b99363302605ad683855d29dc8d87a
Jollyfant <koymans@knmi.nl>
2018-12-19 15:43:04 +0100

    Fix euler pole algorithm: do not skip last age

--- geography/Euler.js
+++ geography/Euler.js
@@ -153,6 +153,7 @@ function convolvePoles(poleOne, poleTwo) {
 
   var M = nullMatrix();
 
+  // Multiple [R1] by [R2]
   for(var i = 0; i < 3; i++) {
     for(var j = 0; j < 3; j++) {
       for(var k = 0; k < 3; k++) {
