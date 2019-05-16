3a920894adc68d501ca91c11ddd1262d1e26d90f
Jollyfant <koymans@knmi.nl>
2019-05-16 20:32:31 +0200

    Change p in to br

--- submit/js/index.js
+++ submit/js/index.js
@@ -1,3 +1,3 @@
 if(window.location.search) {
-  notify("success", "Your publication was succesfully received and was assigned identifier: <b><a href='" +  window.location.search.slice(1)  + "'>" + window.location.search.slice(1) + "</a></b>. <p> It will be reviewed and added to the data library soon.");
+  notify("success", "Your publication was succesfully received and was assigned identifier: <b><a href='" +  window.location.search.slice(1)  + "'>" + window.location.search.slice(1) + "</a></b>. <br> It will be reviewed and added to the data library soon.");
 }
