89757c5eef87d69fb71218cc60d3b231ba22d821
Jollyfant <m.r.koymans@gmail.com>
2020-02-17 16:24:04 +0100

    Use categories for intensity plot

--- interpretation/js/graphs.js
+++ interpretation/js/graphs.js
@@ -108,6 +108,7 @@ function plotIntensityDiagram(hover) {
   var specimen = getSelectedSpecimen();
 
   var intensities = new Array();
+  var categories = new Array();
   var hoverIndex = null;
 
   specimen.steps.forEach(function(step, i) {
@@ -122,11 +123,10 @@ function plotIntensityDiagram(hover) {
       hoverIndex = intensities.length;
     }
 
-    // Get the treatment step as a number
-    var treatmentStep = extractNumbers(step.step);
+    // Use categories to stop mixing AF / TH
+    categories.push(step.step)
 
     intensities.push({
-      "x": treatmentStep,
       "y": new Coordinates(step.x, step.y, step.z).length,
       "stepIndex": i
     });
@@ -217,7 +217,7 @@ function plotIntensityDiagram(hover) {
     "zIndex": 0
   });
 
-  createIntensityDiagram(hover, plotSeries);
+  createIntensityDiagram(hover, plotSeries, categories);
 
 }
 
@@ -834,7 +834,7 @@ function resetMarkerSize() {
 
 }
 
-function createIntensityDiagram(hover, series) {
+function createIntensityDiagram(hover, series, categories) {
 
   /*
    * Function createIntensityDiagram
@@ -895,6 +895,7 @@ function createIntensityDiagram(hover, series) {
       "formatter": intensityTooltip
     },
     "xAxis": {
+      "categories": categories,
       "title": {
         "text": "Demagnetization steps"
       }
