3c4f930956108da0bec116ae555364f370d3516b
Jollyfant <m.r.koymans@gmail.com>
2020-03-02 16:37:21 +0100

    More lenient parsing of Utrecht format; remove quotes

--- interpretation/js/importing.js
+++ interpretation/js/importing.js
@@ -1537,7 +1537,7 @@ function importUtrecht(file) {
   }
 
   // Split by 9999 (Utecht specimen delimiter)
-  var blocks = file.data.split(/9999\r?\n/);
+  var blocks = file.data.split(/9999\s+\r?\n/);
 
   if(blocks.length === 1 || blocks[blocks.length - 1].trim().replace(/"/g,"") !== "END") {
     throw(new Exception("Invalid Utrecht format."));
@@ -1559,6 +1559,7 @@ function importUtrecht(file) {
 
     // Extract the header parameters
     var [sampleName, _, coreAzimuth, coreDip, sampleVolume, beddingStrike, beddingDip] = header.split(/,[\s]*/);
+    sampleName = sampleName.replace(/"/g, "");
 
     var steps = new Array();
 
