f4a327f32f46cc0ffa4ba784c28d1d18a7b25e4c
Jollyfant <koymans@knmi.nl>
2020-08-26 11:54:57 +0200

    Update DOI

--- js/utils.js
+++ js/utils.js
@@ -1,6 +1,6 @@
 let __DEBUG__ = false;
 let __VERSION__ = "2.1.0";
-const __DOI__ = "10.5281/zenodo.3723790"
+const __DOI__ = "10.5281/zenodo.4001578"
 const RADIANS = Math.PI / 180;
 const PROJECTION_TYPE = "AREA";
 const DEGREE_SYMBOL = "\u00B0";
